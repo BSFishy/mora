4e5a2cf6dad1a5fbd1bdbdfa5fdf19aeadc5dc43
Matt Provost <mattprovost6@gmail.com>
2025-07-03 16:15:00 +0000

    chore: add initial set of config options for cloudflared
    the first service im gonna try and spin out is cloudflared.
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- sample/my_module1/custom_image/main.go
+++ sample/my_module1/custom_image/main.go
@@ -13,17 +13,28 @@ func (m *MyModule) GetConfigPoints(ctx context.Context) ([]config.Point, error) 
 	state := wingman.GetState(ctx)
 	module := wingman.ModuleName(ctx)
 
-	if cfg := state.FindConfig(module, "test"); cfg != nil {
-		return []config.Point{}, nil
+	cfp := []config.Point{}
+
+	if cfg := state.FindConfig(module, "api_key"); cfg == nil {
+		description := `Hello <a href="https://dash.cloudflare.com/profile/api-tokens">here</a>.`
+
+		cfp = append(cfp, config.Point{
+			Identifier:  "api_key",
+			Name:        "API Key",
+			Description: &description,
+			Kind:        config.Secret,
+		})
 	}
 
-	return []config.Point{
-		{
-			Identifier: "test",
-			Name:       "Testing",
-			Kind:       config.Secret,
-		},
-	}, nil
+	if cfg := state.FindConfig(module, "email"); cfg == nil {
+		cfp = append(cfp, config.Point{
+			Identifier: "email",
+			Name:       "Email",
+			Kind:       config.String,
+		})
+	}
+
+	return cfp, nil
 }
 
 func main() {

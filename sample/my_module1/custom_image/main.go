0f845e79b720641b8e82a3907d45ce6acdd72c46
Matt Provost <mattprovost6@gmail.com>
2025-07-04 17:52:36 +0000

    feat: use functions in the wingman
    holy canoly. we got functions in the wingmen. now we can define a function to
    get the token for the tunnel (ie create the tunnel if it doesnt exist, then
    return the token for it, or create the tunnel when the wingman starts and just
    return the token when it is called) and we can also register the function to get
    a url and have the zone configured for the root of all of the domains in the
    cluster. super sick.
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- sample/my_module1/custom_image/main.go
+++ sample/my_module1/custom_image/main.go
@@ -3,38 +3,52 @@ package main
 import (
 	"context"
 
-	"github.com/BSFishy/mora-manager/config"
+	"github.com/BSFishy/mora-manager/expr"
+	"github.com/BSFishy/mora-manager/point"
+	"github.com/BSFishy/mora-manager/value"
 	"github.com/BSFishy/mora-manager/wingman"
 )
 
 type MyModule struct{}
 
-func (m *MyModule) GetConfigPoints(ctx context.Context) ([]config.Point, error) {
-	state := wingman.GetState(ctx)
-	module := wingman.ModuleName(ctx)
+func (m *MyModule) GetConfigPoints(ctx context.Context, deps wingman.WingmanContext) ([]point.Point, error) {
+	state := deps.GetState()
+	module := deps.GetModuleName()
 
-	cfp := []config.Point{}
+	cfp := []point.Point{}
 
 	if cfg := state.FindConfig(module, "api_key"); cfg == nil {
 		description := `Hello <a href="https://dash.cloudflare.com/profile/api-tokens">here</a>.`
 
-		cfp = append(cfp, config.Point{
+		cfp = append(cfp, point.Point{
 			Identifier:  "api_key",
 			Name:        "API Key",
 			Description: &description,
-			Kind:        config.Secret,
+			Kind:        point.Secret,
 		})
 	}
 
 	if cfg := state.FindConfig(module, "email"); cfg == nil {
-		cfp = append(cfp, config.Point{
+		cfp = append(cfp, point.Point{
 			Identifier: "email",
 			Name:       "Email",
-			Kind:       config.String,
+			Kind:       point.String,
 		})
 	}
 
 	return cfp, nil
+}
+
+func (m *MyModule) GetFunctions(ctx context.Context, deps wingman.WingmanContext) (map[string]expr.ExpressionFunction, error) {
+	return map[string]expr.ExpressionFunction{
+		"cloudflared_token": {
+			MinArgs: 0,
+			MaxArgs: 0,
+			Evaluate: func(ctx context.Context, deps expr.EvaluationContext, args expr.Args) (value.Value, []point.Point, error) {
+				return value.NewString("hello world!"), nil, nil
+			},
+		},
+	}, nil
 }
 
 func main() {
